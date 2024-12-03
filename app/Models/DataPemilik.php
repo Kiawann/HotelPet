6f15cabe04770f7cd4de8ec05d7f06a1a1f5f8ff
salma fadillah <salmafadillahdwiyanti@gmail.com>
2024-12-03 09:27:36 +0700

    crud kategori + data hewan

--- app/Models/DataPemilik.php
+++ app/Models/DataPemilik.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class DataPemilik extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'data_pemilik'; 
+
+    protected $fillable = [
+        'user_id',
+        'nama',
+        'jenis_kelamin',
+        'nomor_telp',
+        'foto',
+    ];
 }
